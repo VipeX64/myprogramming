757903559931549b55c9e73089188c9e497ad276
VipeX64 <netopyr.64@seznam.cz>
2018-11-18 19:34:41 +0100

    enter

--- Misa/HAX&Gone/src/script/script.js
+++ Misa/HAX&Gone/src/script/script.js
@@ -1,10 +1,22 @@
 
-$(document).keyup(function(e){
-    if(e.which==122){
+let focus = false;
+    $("input").focus(function () {
+        focus = true;
+    })
+    $("input").blur(function () {
+        focus = false;
+    })
+    
+$(document).keyup(function (e) {
+    
+    if (e.which == 122) {
         e.preventDefault();//kill anything that browser may have assigned to it by default
         //do what ever you wish here :) 
-        $("#desktop").css("height","100%");
-        $("#desktop").css("height","-webkit-fill-available");
+        $("#desktop").css("height", "100%");
+        $("#desktop").css("height", "-webkit-fill-available");
         return false;
     }
- });
+    if (e.which == 13 && focus) {
+        alert("works");
+    }
+});
