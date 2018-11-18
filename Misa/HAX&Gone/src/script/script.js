28daeee76940d5d4f509c47a1dbdf1ee3a5f07b4
Michael295639 <michal295639gmail.com>
2018-11-18 21:23:11 +0100

    a

--- Misa/HAX&Gone/src/script/script.js
+++ Misa/HAX&Gone/src/script/script.js
@@ -1,5 +1,5 @@
 
-vstupTerminalu = "";
+celkovyTerminalu = "";
 
 let focus = false;
     $("input").focus(function () {
@@ -8,21 +8,50 @@ let focus = false;
     $("input").blur(function () {
         focus = false;
     })
+
+<<<<<<< HEAD
+function connectFunc(adress) {
+    alert(adress);
+}
     
+=======
+>>>>>>> e85c5c4f8152defcfdbdde39d18098fd22f275ea
 $(document).keyup(function (e) {
     
     if (e.which == 122) {
         e.preventDefault();//kill anything that browser may have assigned to it by default
         //do what ever you wish here :) 
-        $("#desktop").css("height", "100%");
-        $("#desktop").css("height", "-webkit-fill-available");
+        
         return false;
     }
     if (e.which == 13 && focus) {
-        vstupTerminalu += ">";
-        vstupTerminalu += $("#TerminalInput").val();
-        vstupTerminalu += "\n";
-        $("#TerminalOutput").val(vstupTerminalu);
-        alert(vstupTerminalu);
+        vstupTerminalu = $("#TerminalInput").val();
+        celkovyTerminalu += ">";
+        celkovyTerminalu += vstupTerminalu;
+        celkovyTerminalu += "\n";
+        let arrayInputu = vstupTerminalu.split(" ");
+        if (arrayInputu[0] == "connect") {
+            connectFunc(arrayInputu[1]);
+        }
+        $("#TerminalOutput").val(celkovyTerminalu);
     }
 });
+
+function formatDate(date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+  
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+  
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  }
+  $("#date").text(formatDate(new Date()));
+
+//   let now = new Date();
+//   alert( now );
