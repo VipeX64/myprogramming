65089f7922fd298552a37bd2558845bcd837fa82
VipeX64 <netopyr.64@seznam.cz>
2018-11-18 20:46:37 +0100

    grapx

--- Misa/HAX&Gone/src/script/script.js
+++ Misa/HAX&Gone/src/script/script.js
@@ -6,17 +6,35 @@ let focus = false;
     $("input").blur(function () {
         focus = false;
     })
-    
+
 $(document).keyup(function (e) {
     
     if (e.which == 122) {
         e.preventDefault();//kill anything that browser may have assigned to it by default
         //do what ever you wish here :) 
-        $("#desktop").css("height", "100%");
-        $("#desktop").css("height", "-webkit-fill-available");
+        
         return false;
     }
     if (e.which == 13 && focus) {
         alert("works");
     }
 });
+
+function formatDate(date) {
+    var monthNames = [
+      "January", "February", "March",
+      "April", "May", "June", "July",
+      "August", "September", "October",
+      "November", "December"
+    ];
+  
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+  
+    return day + ' ' + monthNames[monthIndex] + ' ' + year;
+  }
+  $("#date").text(formatDate(new Date()));
+
+//   let now = new Date();
+//   alert( now );
